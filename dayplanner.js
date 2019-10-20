d5a8172583c55753497b0ff6445fe637d597828a
Daniele Esposito <esp.dne@gmail.com>
2019-10-20 22:16:41 +0200

    local storage missing colours

--- dayplanner.js
+++ dayplanner.js
@@ -1,6 +1,23 @@
 
 // date on the title
-var dateOfToday = moment().format('LLLL');; 
-console.log(dateOfToday);
+var dateOfToday = moment().format('MMMM Do YYYY, h:mm a'); 
+var timeControl = moment().format('LT').value;
 $('#currentDay').append('<p>').text(dateOfToday);
+
+
+// change colours: if it is time green future other colour
+//console.log(timeControl);
+// if (timeControl ==
+
+
+// Local Storage
+var saveButton = document.querySelector("#myBtn");
+saveButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var inputValue = document.getElementById("input").value;
+
+    localStorage.setItem("input", JSON.stringify(inputValue));
+
+    /// render the infos of the storage
+});
 
